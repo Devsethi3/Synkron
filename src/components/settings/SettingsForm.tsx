aa887f24f70f96c89ae1b3a6046d6cc79420b8eb
Devsethi3 <devsethi4522@gmail.com>
2024-04-27 20:31:19 +0530

    Updating Settings

--- src/components/settings/SettingsForm.tsx
+++ src/components/settings/SettingsForm.tsx
@@ -1,9 +1,122 @@
-import React from 'react'
+"use client";
+
+import { useAppState } from "@/providers/StateProvider";
+import { useToast } from "../ui/use-toast";
+import { useRef, useState } from "react";
+import { User, workspace } from "@/lib/supabase/supabase.types";
+import { useSupabaseUser } from "@/providers/SupabaseUserProvider";
+import { useRouter } from "next/navigation";
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { Briefcase } from "lucide-react";
+import { Label } from "@radix-ui/react-label";
+import { Separator } from "../ui/separator";
+import { v4 } from "uuid";
+import { updateWorkspace } from "@/lib/supabase/queries";
+import { Input } from "../ui/input";
 
 const SettingsForm = () => {
-  return (
-    <div>SettingsForm</div>
-  )
-}
+  const { toast } = useToast();
+  const { user } = useSupabaseUser();
+  const router = useRouter();
+  const supabase = createClientComponentClient();
 
-export default SettingsForm
+  const { state, workspaceId, dispatch } = useAppState();
+  const [permissions, setPermissions] = useState("Private");
+  const [collaborators, setCollaborators] = useState<User[] | []>([]);
+  const [openAlertMessage, setOpenAlertMessage] = useState(false);
+  const [workspaceDetails, setWorkspaceDetails] = useState<workspace>();
+  const titleTimerRef = useRef<ReturnType<typeof setTimeout>>();
+  const [uploadProfilePic, setUploadProfilePic] = useState(false);
+  const [uploadingLogo, setUploadingLogo] = useState(false);
+  const [loadingPortal, setLoadingPortal] = useState(false);
+
+  // Add collaborators
+
+  // Remove Collaborators
+
+  // Onchange workspace title
+  const workspaceNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!workspaceId || !e.target.value) return;
+    dispatch({
+      type: "UPDATE_WORKSPACE",
+      payload: { workspace: { title: e.target.value }, workspaceId },
+    });
+    if (titleTimerRef.current) clearTimeout(titleTimerRef.current);
+    titleTimerRef.current = setTimeout(async () => {
+      // await updateWorkspace({ title: e.target.value }, workspaceId);
+    }, 500);
+  };
+
+  const onChangeWorkspaceLogo = async (
+    e: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    if (!workspaceId) return;
+    const file = e.target.files?.[0];
+    if (!file) return;
+    const uuid = v4();
+    setUploadingLogo(true);
+    const { data, error } = await supabase.storage
+      .from("workspace-logos")
+      .upload(`workspaceLogo.${uuid}`, file, {
+        cacheControl: "3600",
+        upsert: true,
+      });
+
+    if (!error) {
+      dispatch({
+        type: "UPDATE_WORKSPACE",
+        payload: { workspace: { logo: data.path }, workspaceId },
+      });
+      await updateWorkspace({ logo: data.path }, workspaceId);
+      setUploadingLogo(false);
+    }
+  };
+
+  // onClick
+
+  // Fetching avatar details
+
+  // Get Workspace Details
+
+  // Get alll the collaborators
+
+  return (
+    <div className="flex gap-4 flex-col">
+      <p className="flex items-center gap-2 mt-6">
+        <Briefcase size={20} />
+        Workspace
+      </p>
+      <Separator />
+      <div className="flex flex-col gap-2">
+        <Label
+          htmlFor="workspaceName"
+          className="text-sm text-muted-foreground"
+        >
+          Name
+        </Label>
+        <Input
+          name="workspaceName"
+          value={workspaceDetails ? workspaceDetails.title : ""}
+          placeholder="Workspace Name"
+          onChange={workspaceNameChange}
+        />
+        <Label
+          htmlFor="workspaceLogo"
+          className="text-sm text-muted-foreground"
+        >
+          Workspace Logo
+        </Label>
+        <Input
+          name="workspaceLogo"
+          type="file"
+          accept="image/*"
+          placeholder="Workspace Logo"
+          onChange={onChangeWorkspaceLogo}
+          disabled={uploadingLogo}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default SettingsForm;
