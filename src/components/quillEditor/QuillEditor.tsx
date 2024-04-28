338d0a57fd6606a57dd721eba9496df5c7c8306a
Devsethi3 <devsethi4522@gmail.com>
2024-04-28 22:43:56 +0530

    Trash and Delete in Quill Editor

--- src/components/quillEditor/QuillEditor.tsx
+++ src/components/quillEditor/QuillEditor.tsx
@@ -32,7 +32,7 @@ var TOOLBAR_OPTIONS = [
   [{ size: ["small", false, "large", "huge"] }], // custom dropdown
   [{ header: [1, 2, 3, 4, 5, 6, false] }],
 
-  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ color: [] }, { background: [] }], 
   [{ font: [] }],
   [{ align: [] }],
 
