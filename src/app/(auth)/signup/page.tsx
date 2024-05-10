8eae7f3065c69e7383b199ac2d94f1c3e153146a
Devsethi3 <devsethi4522@gmail.com>
2024-05-10 19:36:34 +0530

    Fixing Auth Errors

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -58,7 +58,7 @@ const Signup = () => {
       param.includes("error_description=")
     );
     if (!errorParam) return "";
-    return decodeURIComponent(errorParam.split("=")[1]); // Extract error
+    return decodeURIComponent(errorParam.split("=")[1]); 
   }, [searchParams]);
 
   const confirmationAndErrorStyles = useMemo(
